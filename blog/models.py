4398066a537ae60961190ee51d4ac7f6ae3179f7
Taeyoung Park <taeyoung613@gmail.com>
2016-06-08 14:45:01 +0900

    모델 전체 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+    photo = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+                return self.name
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=40)
+    address = models.CharField(max_length=500)
+    description = models.TextField()
+    photo1 = models.ImageField()
+    photo2 = models.ImageField(null=True, blank=True)
+    photo3 = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+                return self.name
+
+class Review(models.Model):
+    shop = models.ForeignKey(Shop)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    comment = models.TextField()
+    photo1 = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+                return self.pk
--- final/urls.py
+++ final/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^blog/',include("blog.urls",namespace="blog"))
+    url(r'^$', lambda request: redirect('/blog/')),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
